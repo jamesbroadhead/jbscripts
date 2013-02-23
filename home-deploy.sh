7f7b00eb5f817206e2e1671c7bbfcbea5f70d6a8
James Broadhead <jamesbroadhead@gmail.com>
2013-02-23 12:49:20 +0000

    Major rewrite of home-deploy.sh, supporting nested dirs now

--- home-deploy.sh
+++ home-deploy.sh
@@ -1,34 +1,71 @@
 #!/bin/bash
 
-DEPLOYDIR="$1"
+DEPLOY_FROM_DIR="$1"
+bak="${HOME}/bak/$(date +%F)"
 
-for i in $1/dot_* ; do
-    dot_name="$(basename ${i})"
-    dot_path="$(dirname ${i})/${dot_name}"
-    newname="$(basename ${i/dot_/.})"
-    newpath="${HOME}/${newname}"
+home_form() {
+	# convert a <>/inside_dot_foo -> ~/.foo path
+	home_form=$(echo "$1" | sed 's|.*dot_|'"$HOME"'/.|')
+	echo ${home_form}
+}
 
-    if [ -e "${newpath}" ] || [ -L "${newpath}" ] ; then 
-	
-	# broken symlink in place
-	if ! [ -f "${HOME}/$newname" ] && [ -L "${HOME}/$newname" ] ; then 
-		rm "$newpath"
-	# not a symlink pointing to the correct location
-	elif ! [ "$(readlink ${HOME}/${newname})" = "${dot_path}" ] ; then
-		echo "$newname already existed! $newname --> ${newname}.bak"
-		mv -i "${HOME}/${newname}" "${HOME}/${newname}.bak"
-	
-	# for simplicity, remove symlinks pointing to the correct location
-   	else
-		rm $newpath
+make_symlink() {
+	# $1 : full path to a file which should be symlinked into ~
+    dot_path="${1}"
+	dot_name="$(basename ${1})"
+	newpath=$(home_form $dot_path)
+   	
+	if [ -L "${newpath}" ] ; then 
+		# broken symlink in place
+		if ! [ -f "${newpath}" ] && [ -L "${newpath}" ] ; then 
+			#echo "would remove $newpath"	
+			rm "$newpath"
+		# not a symlink pointing to the correct location
+		elif ! [ "$(readlink ${newpath})" = "${dot_path}" ] ; then
+			#echo "would replace $newpath"
+			backup_old_content $newpath
+		# there is a symlink pointing to the correct location already	
+		else
+			#echo "looks ok $newpath"	
+			return 0
+		fi
+		
+	elif [ -e "${newpath}" ] ; then
+		#echo "would backup $newpath"	
+		backup_old_content $newpath
 	fi
-    fi
+	
+	# there is no file or symlink at $newname already
+	
+	#echo "would link $newpath"
+	mkdir -p $(dirname $newpath)
+	ln -s "$dot_path" "$newpath"
+}
 
-    ln -s "$dot_path" ${HOME}/${newname}
+# remove $1 from $HOME, so that a symlink can be created
+backup_old_content() {
+	# $1 filename in $HOME which should be moved to $bakpath
+	path="$1"
+	bakpath="$bak/$path"
+	mkdir -p "$(dirname $bakpath)"
+	echo "$path already existed! --> $bakpath"
+	mv -i "$path" "${bakpath}"
+}
+
+confirmfile="$DEPLOY_FROM_DIR/.is_home_deploy_dir"
+if ! [ -f $confirmfile ] ; then
+	echo "Didn't find $confirmfile -- exiting"
+	exit 1
+fi
+
+# cleanup from older versions
+for e in $DEPLOY_FROM_DIR/* ; do
+	base=$(basename $e)
+	if [ -d $e ] && [ -L $HOME/$base ] ; then
+		echo "Symlinks to deployable dirs exist in your homedir. Bailing"
+	fi
 done
 
-echo "==============="
-echo " need manual attention: "
-for i in $1/inside* ; do
-	echo "$i"
+for abspath in $(find "$1" -wholename "*dot_*" -type f -exec readlink -f {} \;) ; do
+	make_symlink "${abspath}"
 done
